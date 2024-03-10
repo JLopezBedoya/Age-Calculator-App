1c7098ce5df3f684ad4c4a6bb48d620ad3553a1b
JLopezBedoya <jlopezbedoya1011@gmail.com>
2024-03-10 12:08:19 -0500

    basics

--- src/components/AgeCalculator.vue
+++ src/components/AgeCalculator.vue
@@ -1,58 +1,72 @@
 <script setup>
 import { ref } from 'vue';
-
+    const day = ref(0)
+    const month = ref(0)
+    const year = ref(0)
     const time = ref({
-        day: "--",
-        month: "--",
-        year: "--"
+        day: "29",
+        month: "11",
+        year: "86"
     })
+    const Datehandle = ()=>{
+        const current = new Date()
+        time.value.year = current.getFullYear() - year.value
+        time.value.month = current.getMonth() - (month.value-1)
+        time.value.day = current.getDate() - day.value
+    }
 </script>
 
 <template>
 <div class="bg-purple-200 h-screen w-screen flex justify-center items-center">
-    <section class="md:w-[50%] md:h-[75%] card p-5 grid grid-cols-1 grid-rows-6 bg-white font-bold">
+    <section class="h-[60%] w-[90%] sm:w-[55%] sm:h-[70%] hz card p-5 grid grid-cols-1 grid-rows-6 bg-white font-bold">
 
-        <article class="flex justify-between w-[80%]">
+        <article class="flex md:w-[80%] w-[100%]">
             <section>
                 <p class="mb-1">DAY</p>
-                <input class="p-2 text-2xl w-[70%] rounded-md border-purple-200 border-2" type="text" placeholder="DD">
+                <input v-model="day" class="p-2 text-lg md:text-2xl w-[75%] rounded-md border-purple-200 border-2" type="text" placeholder="DD">
             </section>
             <section>
                 <p class="mb-1">MONTH</p>
-                <input class="p-2 text-2xl w-[70%] rounded-md border-purple-200 border-2" type="text" placeholder="MM">
+                <input v-model="month" class="p-2 text-lg md:text-2xl w-[75%] rounded-md border-purple-200 border-2" type="text" placeholder="MM">
             </section>
             <section>
                 <p class="mb-1">YEAR</p>
-                <input class="p-2 text-2xl w-[70%] rounded-md border-purple-200 border-2" type="text" placeholder="YYYY">
+                <input v-model="year" class="p-2 text-lg md:text-2xl w-[75%] rounded-md border-purple-200 border-2" type="text" placeholder="YYYY">
             </section>
         </article>
 
         <article class="grid place-items-center relative w-[90%]">
-            <button class="absolute right-0 bg-purple-600 hover:bg-purple-800 active:bg-purple-900 max-h-20 max-w-20 h-20 w-20 rounded-full text-white text-6xl">&#10549;</button>
+            <button @click="Datehandle" class="absolute self-center md:right-0 bg-purple-600 hover:bg-purple-800 active:bg-purple-900 max-h-14 max-w-14 h-14 w-14 md:max-h-20 md:max-w-20 md:h-20 md:w-20 rounded-full text-white text-4xl md:text-5xl">&#10549;</button>
             <div class="border border-purple-100 border-t-2 w-[100%]"></div>
         </article>
 
-        <article class="text-7xl grid grid-rows-3 row-span-4">
-            <p class="italian"><span class="text-purple-600">{{ time.year }}</span> Years</p>
-            <p class="italian"><span class="text-purple-600">{{ time.month }}</span> months</p>
-            <p class="italian"><span class="text-purple-600">{{ time.day }}</span> days</p>
+        <article class="md:text-7xl text-5xl grid grid-rows-3 row-span-4">
+            <p class="italian"><span class="italian text-purple-800">{{ time.year }}</span> years</p>
+            <p class="italian"><span class="italian text-purple-800">{{ time.month }}</span> months</p>
+            <p class="italian"><span class="italian text-purple-800">{{ time.day }}</span> days</p>
         </article>
     </section>
 </div>
 </template>
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;1,500&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,700&display=swap');
     *{
-        font-family: "Montserrat", sans-serif;
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
         font-style: normal;
     }
     .card{
-        border-radius: 20px 20px 200px 20px;
+        border-radius: 20px 20px 150px 20px;
     }
     .italian{
-        font-family: "Montserrat", sans-serif;
-        font-style: normal;
+        font-family: "Poppins", sans-serif;
+        font-weight: 700;
         font-style: italic;
+    }
+    @media screen and (min-width: 768px) and (max-width: 1023px) and (max-height: 500px){
+        .hz{
+            height: 100%;
+        }
     }
 </style>
